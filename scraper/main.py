26ccd5e8ac3138669ba9c02ceea5c3454796f466
Thomas Barker <thomasbarker2011@hotmail.co.uk>
2021-02-04 16:46:33 +0000

    Issue #2 added ability to scrape all tags from stack overflow into an csv file.

--- scraper/main.py
+++ scraper/main.py
@@ -2,18 +2,40 @@ from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
 
-
-
-
 def main():
+    tags_left = True
     url = "https://stackoverflow.com/tags"
-    page = urlopen(url)
-    html = page.read().decode("utf-8")
-    soup = BeautifulSoup(html, "html.parser")
-    tags = soup.findAll("a","post-tag")
-    #prints all tags on the webpage
-    for tag in tags:
-        print(tag.string)
+    #url = "https://stackoverflow.com/tags?page=1779&tab=popular"
+    filename = "tag-test.csv"
+
+    while tags_left:
+        page = urlopen(url)
+        html = page.read().decode("utf-8")
+        soup = BeautifulSoup(html, "html.parser")
+        tags = soup.findAll("a", "post-tag")
+        tag_list = [tag.string for tag in tags]
+        # process tags
+        writeStackTags(tag_list, filename)
+
+        # check if another page exists then updates url if so
+        nextpage = soup.find("a", "s-pagination--item js-pagination-item", rel="next")
+        if nextpage is None:
+            tags_left = False
+        else:
+            url = "https://stackoverflow.com" + nextpage["href"]
+
+
+# takes a list of tag strings and writes them to a file
+def writeStackTags(tags, filename):
+    try:
+        f = open(filename, "a")
+        for tag in tags:
+            f.write(tag + ",")
+        f.write("\n")
+    except FileNotFoundError:
+        print("Unable to open file " + filename)
+    finally:
+        f.close()
 
 if __name__ == "__main__":
     main()
