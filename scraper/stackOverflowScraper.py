0a0d0e5c23af2016c55bb6dbf13cf82dc45eb4f3
Thomas Barker <thomasbarker2011@hotmail.co.uk>
2021-02-06 21:40:32 +0000

    Issue #2 posts can be scraped from multiple pages at once now

--- scraper/stackOverflowScraper.py
+++ scraper/stackOverflowScraper.py
@@ -17,32 +17,36 @@ class StackOverflowPost:
         self._answers = [" ".join([line.text for line in answer.findAll("p")]) for answer in
                          soup.findAll("div", "answer")]
 
-        #todo add any additional and convert into a format wanted by fasttext
+        # todo add any additional and convert into a format wanted by fasttext
 
     def __str__(self):
         return self._title
 
 
-class StackOverflowPostScraper:
+def getStackOverflowPosts(url, max_num_pages):
+    page_num = 0
 
-    def __init__(self):
-        post_page_url = "https://stackoverflow.com/questions?tab=Votes"
-
-        html = urlopen(post_page_url).read().decode("utf-8")
+    while page_num < max_num_pages:
+        html = urlopen(url).read().decode("utf-8")
         soup = BeautifulSoup(html, "html.parser")
+
         links = ["https://stackoverflow.com" + question["href"] for question in
                  soup.find(id="questions").find_all(class_="question-hyperlink")]
 
-        posts = (StackOverflowPost(link) for link in links)
-        print(posts)
+        for link in links:
+            yield StackOverflowPost(link)
 
-        for post in posts:
-            print(post)
-        print("Scraped posts")
+        # check if another page exists then updates url if so
+        next_page = soup.find("a", "s-pagination--item js-pagination-item", rel="next")
+        if next_page is None:
+            break
+        else:
+            url = "https://stackoverflow.com" + next_page["href"]
+            page_num += 1
+        print("next_page")
 
 
-def generateStackOverflowTags(url):
-    max_num_pages = 2
+def getStackOverflowTags(url, max_num_pages):
     page_num = 0
 
     while page_num < max_num_pages:
@@ -63,9 +67,10 @@ def generateStackOverflowTags(url):
 
 
 if __name__ == "__main__":
-    StackOverflowPostScraper()
+    for post in getStackOverflowPosts("https://stackoverflow.com/questions?tab=Votes",2):
+        print(post)
 
-    #todo error catching for incorrect website links and testing
+    # todo error catching for incorrect website links and testing
     tag_page_url = "https://stackoverflow.com/tags"
-    for tag in generateStackOverflowTags(tag_page_url):
+    for tag in getStackOverflowTags(tag_page_url,2):
         print(tag)
