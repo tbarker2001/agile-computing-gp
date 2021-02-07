1d04b08fac56937bdf6d2653a2223d95db503217
Thomas Barker <thomasbarker2011@hotmail.co.uk>
2021-02-07 21:47:33 +0000

    Issue #2 tags are no longer repeated and general clean_up of code including addition of getters for stackOverflowPost

--- scraper/stackOverflowScraper.py
+++ scraper/stackOverflowScraper.py
@@ -9,15 +9,30 @@ class StackOverflowPost:
         html = urlopen(url).read().decode("utf-8")
         soup = BeautifulSoup(html, "html.parser")
 
-        self._post_tags = [tag.text for tag in soup.findAll("a", "post-tag")]
-        self._title = soup.find(id="question-header").find("a", "question-hyperlink").string
+        self._post_tags = [tag.text for tag in soup.find(class_="post-taglist").findAll(class_="post-tag")]
+        self._title = soup.find(id="question-header").find(class_="question-hyperlink").string
 
-        self._post = " ".join([line.text for line in soup.find("div", "s-prose js-post-body").findAll("p")])
+        post_lines = [line.text for line in soup.find(class_="s-prose js-post-body").findAll("p")]
+        self._post = " ".join(post_lines)
 
-        self._answers = [" ".join([line.text for line in answer.findAll("p")]) for answer in
-                         soup.findAll("div", "answer")]
+        self._answers = []
+        for answer in soup.findAll(class_="answer"):
+            answer_lines = [line.text for line in answer.findAll("p")]
+            self._answers.append(" ".join(answer_lines))
 
-        # todo add any additional and convert into a format wanted by fasttext
+        # todo more comprehensive cleanup of freetext
+
+    def getPostTags(self):
+        return self._post_tags
+
+    def getTitle(self):
+        return self._title
+
+    def getPost(self):
+        return self._post
+
+    def getAnswers(self):
+        return self._answers
 
     def __str__(self):
         return self._title
@@ -28,14 +43,14 @@ def getStackOverflowPosts(url):
         html = urlopen(url).read().decode("utf-8")
         soup = BeautifulSoup(html, "html.parser")
 
-        links = ["https://stackoverflow.com" + question["href"] for question in
-                 soup.find(id="questions").find_all(class_="question-hyperlink")]
+        links_html = soup.find(id="questions").find_all(class_="question-hyperlink")
+        links = ["https://stackoverflow.com" + question["href"] for question in links_html]
 
         for link in links:
             yield StackOverflowPost(link)
 
         # check if another page exists then updates url if so
-        next_page = soup.find("a", "s-pagination--item js-pagination-item", rel="next")
+        next_page = soup.find(class_="s-pagination--item js-pagination-item", rel="next")
         if next_page is None:
             break
         else:
@@ -48,11 +63,11 @@ def getStackOverflowTags(url):
         html = page.read().decode("utf-8")
         soup = BeautifulSoup(html, "html.parser")
 
-        for tag in soup.findAll("a", "post-tag"):
+        for tag in soup.findAll(class_="post-tag"):
             yield tag.string
 
         # check if another page exists then updates url if so
-        next_page = soup.find("a", "s-pagination--item js-pagination-item", rel="next")
+        next_page = soup.find("a", class_="s-pagination--item js-pagination-item", rel="next")
         if next_page is None:
             break
         else:
@@ -60,13 +75,14 @@ def getStackOverflowTags(url):
 
 
 if __name__ == "__main__":
-
-    posts = getStackOverflowPosts("https://stackoverflow.com/questions?tab=Votes")
-
+    post_url = "https://stackoverflow.com/questions?tab=Votes"
+    posts = getStackOverflowPosts(post_url)
+    for i in range(5):
+        print(posts.__next__())
     # note we shouldnt really put the posts straight into a list but instead generate them only as needed
-    bounded_posts = [posts.__next__() for _ in range(5)]
-    for post in bounded_posts:
-        print(post)
+    # bounded_posts = [posts.__next__() for _ in range(5)]
+    # for post in bounded_posts:
+    # print(post)
 
     # todo error catching for incorrect website links and testing
     tag_page_url = "https://stackoverflow.com/tags"
