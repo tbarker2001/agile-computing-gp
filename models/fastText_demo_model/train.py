1d058634777a53efcbf96b33cafb392fcd99d120
Gabor Pituk <pitukgabor@gmail.com>
2021-02-09 15:53:30 +0100

    Add scripts for validation and prediction

--- models/fastText_demo_model/train.py
+++ models/fastText_demo_model/train.py
@@ -1,10 +1,8 @@
 import fasttext
 
 # Train supervised model from labelled StackOverflow threads
-model = fasttext.train_supervised('stackoverflowdata.txt')
+model = fasttext.train_supervised('stackoverflowdata.train.txt', epoch=25, lr=1.0, wordNgrams=2, verbose=2, minCount=1)
 
-# Test the model on a random text
-prediction = model.predict("Who can I ask for advice on git workflow and how to use yield in Python?", k=5)
-
-print("\nPrediction for \"Who can I ask for advice on git workflow and how to use yield in Python?\": " + str(prediction[0]) + " with probabilities: " + str(prediction[1]))
+# Save model to file
+model.save_model('model.fasttextmodel')
 
---
+++ models/fastText_demo_model/validate.py
@@ -0,0 +1,15 @@
+import fasttext
+
+# Load model from file
+model = fasttext.load_model('model.fasttextmodel')
+
+# Prints test result
+def print_results(N, p, r):
+    print("N\t" + str(N))
+    print("P@{}\t{:.3f}".format(1, p))
+    print("R@{}\t{:.3f}".format(1, r))
+
+# Test model on validation dataset
+test_result = model.test('stackoverflowdata.validate.txt', k=5)
+print_results(*test_result)
+
