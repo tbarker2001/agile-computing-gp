853b03b50dbd44c69faa0d45d7fb0ce9066b669b
Gabor Pituk <pitukgabor@gmail.com>
2021-02-11 13:04:28 +0100

    Add quantisation to model & fix test validation result format

--- models/fastText_demo_model/validate.py
+++ models/fastText_demo_model/validate.py
@@ -6,8 +6,8 @@ model = fasttext.load_model('model.fasttextmodel')
 # Prints test result
 def print_results(N, p, r):
     print("N\t" + str(N))
-    print("P@{}\t{:.3f}".format(1, p))
-    print("R@{}\t{:.3f}".format(1, r))
+    print("P@{}\t{:.3f}".format(5, p))
+    print("R@{}\t{:.3f}".format(5, r))
 
 # Test model on validation dataset
 test_result = model.test('stackoverflowdata.validate.txt', k=5)
