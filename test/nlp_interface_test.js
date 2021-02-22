5f974ecdb667f24181a7719e51b477534a18b09d
Gabor Pituk <pitukgabor@gmail.com>
2021-02-22 22:03:16 +0100

    Unit test rest of API

--- test/nlp_interface_test.js
+++ test/nlp_interface_test.js
@@ -1,18 +1,18 @@
 var assert = require('assert');
-const { processProfile, processTask } = require('../nlp_interface.js');
+const { processProfile, processTask, overrideTaskLabels, calculateMatchScores } = require('../nlp_interface.js');
 
 describe('NLP pipeline', () => {
-  describe('#processProfile', () =>
-    it('should return a list of labels for the scraped profile', () =>
+  describe('#processProfile', () => {
+    it('should return a list of labels for the scraped profile', () => {
       processProfile({
 	    "username": "oli1",
 	    "stack_profile": "https://stackoverflow.com/users/12870/oli"
       })
       .then(labels => labels["model_output"][0]["label"])
-    )
-  );
+    })
+  });
       
-  describe('#processTask', () =>
+  describe('#processTask', () => {
     it('should return a list of labels', () =>
       processTask({
 	"text": "When we push commits on to a new branch the git history is always broken",
@@ -21,6 +21,42 @@ describe('NLP pipeline', () => {
       })
       .then(labels => labels["model_output"][0]["label"])
     )
-  )
+
+  })
+
+
+  describe('#overrideTaskLabels', () => {
+    it('should not throw', () =>
+      // TODO
+      overrideTaskLabels({})
+    )
+  })
+
+  describe('#calculateMatchScores', () => {
+    it('should calculate scores with a single pair', () => {
+      const tasks = {
+	  "task1": [
+	      {
+		  "label": "apple",
+		  "probability": 0.7
+	      }
+	  ]
+      };
+      const profiles = {
+	  "acc1": [
+	      {
+		  "label": "apple",
+		  "probability": 0.8
+	      }
+	  ]
+      };
+      calculateMatchScores(tasks, profiles)
+      .then(scores => {
+	assert.equal(typeof scores["account_set"]["acc1"]["task1"]["score"], 'double');
+	assert.equal(scores["account_set"]["acc1"]["task1"], scores["task_set"]["task1"]["acc1"])
+      })
+    })
+
+  })
 });
 
