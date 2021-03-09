e1ab15fb603ff24a38630f7dd73bfa9543b57371
kimxworrall <kimsworrall@gmail.com>
2021-03-09 08:25:18 +0000

    added isadmin property to iseditable

--- src/components/tasks-list.component.js
+++ src/components/tasks-list.component.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import Cookies from 'js-cookie';
 import axios from 'axios';
 
-import Editable from "./editable-title.component";
+import Editabletitle from "./editable-title.component";
 
 const Project = props =>(
   <div> 
@@ -61,9 +61,10 @@ export default class TasksList extends Component {
       closed_tasks: [],
       username: username,
       user_id: '',
+      is_admin:false,
       logged_in: logged_in,
       scores: {},
-      projectname:'',
+      projectname:'Project',
       projectid:0
     }
 
@@ -98,7 +99,7 @@ export default class TasksList extends Component {
 	.catch(console.error);
   axios.get('http://localhost:5000/users/get_user' + this.state.username)
 	.then(res => set_state({
-	  user_id: res.data
+	  is_admin: res.data.is_admin
 	}))
 	.catch(console.error);
     axios.get('http://localhost:5000/projects/'+this.state.projectid)
@@ -241,28 +242,33 @@ export default class TasksList extends Component {
     
 }
 
+
   render() {
+    
     return (
       <div>
 
       <div >  
       <div style = {{float:'left'}}>
-      <h3>  <Editable
-      text={this.state.projectname}
+      <h3>  <Editabletitle
+      text={this.state.projectname+" "}
       placeholder="Project "
       type="input"
-      loggedin = "false"
+      loggedin = {(this.state.logged_in).toString()}
+      isadmin = {(this.state.is_admin).toString()}
       >
       
       <input
         type="text"
         name="task"
-        placeholder="Project "
-        loggedin = "true"
+        placeholder={this.state.projectname+" "}
+        loggedin = {(this.state.logged_in).toString()}
+        isadmin = {(this.state.is_admin).toString()}
         value={this.state.projectname}
+        onChange={(event) => this.setState({projectname: event.target.value })}
         onBlur={this.changeProjectnameHandler}
       />
-    </Editable></h3>
+    </Editabletitle></h3>
     </div>
     <div style = {{float:'left'}}>
     <span class="glyphicon">&#x270f;</span>
