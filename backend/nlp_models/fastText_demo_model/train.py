5df40b2081a9b0c67eac542296528b115b6a65e7
kim worrall <kimsworrall@gmail.com>
2021-03-11 06:01:51 +0000

    Update train.py

--- backend/nlp_models/fastText_demo_model/train.py
+++ backend/nlp_models/fastText_demo_model/train.py
@@ -6,7 +6,7 @@ trainFileName = os.path.join(modelDir, 'stackoverflowdata.train.txt')
 modelFileName = os.path.join(modelDir, 'model.fasttextmodel')
 
 # Train supervised model from labelled StackOverflow threads
-model = fasttext.train_supervised(trainFileName, epoch=25, lr=1.0, wordNgrams=2, verbose=2, minCount=1)
+model = fasttext.train_supervised(trainFileName, epoch=30, lr=1.0, wordNgrams=3, verbose=2, minCount=1,bucket=200000, dim=50, loss='hs')
 
 # Quantize model to reduce size
 model.quantize(input=trainFileName, qnorm=True, retrain=True, cutoff=100000)
